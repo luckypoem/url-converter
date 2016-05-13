78e7c0f9222c57c29359f06434d74179be3e344d
note286 <note286@foxmail.com>
2016-05-13 16:11:07 +0800

    增加 UI 版本的错误提示信息

--- url_converter_ui.py
+++ url_converter_ui.py
@@ -6,10 +6,11 @@ root.title('url-converter')
 # 设置窗口标题
 
 def convert():
-    url_qqxf_output.delete(1.0, tkinter.END)
-    # 清空文本框内容
     url = url_input.get(1.0, tkinter.END)[: -1]
     # 获取用户输入
+    
+    clear(0)
+    # 清空除输入框以外的文本框
     
     if len(url) == 0:
     # 如果输入为空, 则直接返回
@@ -19,35 +20,33 @@ def convert():
     # 计算真实地址
     if url_true == 0:
     # 无法正确转换用户输入的地址
-        url_true = '输入不合法'.encode('utf-8')
+        status_text.set('输入地址有误, 无法转换!')
+        # 错误提示
     else:
     # 能正确地转换用户输入的地址
-        url_true_output.delete(1.0, tkinter.END)
-        # 清空之前的真实地址内容
         url_true_output.insert(tkinter.END, url_true.decode('utf-8'))
         # 显示真实地址
-        
-        url_qqxf_output.delete(1.0, tkinter.END)
-        # 清空之前的 QQ 旋风地址内容
         url_qqxf_output.insert(tkinter.END, url_converter_lib.qqxf(url_true).decode('utf-8'))
         # 显示 QQ 旋风地址
-
-        url_thunder_output.delete(1.0, tkinter.END)
-        # 清空之前的迅雷地址内容
         url_thunder_output.insert(tkinter.END, url_converter_lib.thunder(url_true).decode('utf-8'))
         # 显示迅雷地址
-
-        url_flashget_output.delete(1.0, tkinter.END)
-        # 清空之前的快车地址内容
         url_flashget_output.insert(tkinter.END, url_converter_lib.flashget(url_true).decode('utf-8'))
         # 显示快车地址
+        status_text.set('转换成功!')
+        # 状态信息提示
 
-def clear():
-    url_input.delete(1.0, tkinter.END)
+def clear(input_flag=1):
+    """
+    清空文本框内容, 根据传入的标志位判断是否清空全部
+    """
+    if input_flag == 1:
+    # 如果标志位为 1 则全部清空, 否则输入文本框不清空
+        url_input.delete(1.0, tkinter.END)
     url_true_output.delete(1.0, tkinter.END)
     url_qqxf_output.delete(1.0, tkinter.END)
     url_thunder_output.delete(1.0, tkinter.END)
     url_flashget_output.delete(1.0, tkinter.END)
+    status_text.set('')
         
 convert_button = tkinter.Button(text='转换', command=convert)
 convert_button.grid(row=1, column=1)
@@ -90,6 +89,11 @@ url_flashget_label.grid(row=6, column=1)
 url_flashget_output = tkinter.Text(width=50, height=5)
 url_flashget_output.grid(row=6, column=2)
 # 快车地址输出
+
+status_text = tkinter.StringVar()
+status = tkinter.Entry(text=status_text)
+status.grid(row=7,column=2)
+# 状态信息
 
 tkinter.mainloop()
 # 进入主事件循环
